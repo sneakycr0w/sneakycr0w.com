477f42499122b0d992c20554866d2212292a2f20
Zachary E. Sohovich <zacharyesohovich@gmail.com>
2018-02-08 20:21:47 -0800

    Created Contact Form

---
+++ src/components/ContactForm/index.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { Form, Input, Icon, Button, Row, Col } from 'antd';
+const FormItem = Form.Item;
+const { TextArea } = Input;
+
+class ContactForm extends Component {
+
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Form onSubmit={this.handleSubmit}>
+      <Row gutter={16}>
+        <Col span={6}>
+          <FormItem>
+            {getFieldDecorator('email', {
+              rules: [{ required: true, message: 'Please input your email!' }],
+            })(
+              <Input prefix={<Icon type="mail" />} placeholder="Email" />
+            )}
+          </FormItem>
+        </Col>
+        <Col span={6}>
+          <FormItem>
+            {getFieldDecorator('name', {
+              rules: [{ required: true, message: 'Please input your name!'}],
+            })(
+              <Input prefix={<Icon type="user" />} placeholder="Name" />
+            )}
+          </FormItem>
+        </Col>
+      </Row>
+        <Row gutter={16}>
+          <Col span={12}>
+            <FormItem>
+              {getFieldDecorator('message', {
+                rules: [{ required: true, message: 'Please input a message!'}],
+              })(
+                <TextArea rows={4} placeholder="Message" />
+              )}
+            </FormItem>
+            <FormItem>
+              <Button type="primary" htmlType="submit">Submit</Button>
+            </FormItem>
+          </Col>
+        </Row>
+      </Form>
+    )
+  }
+}
+
+export default Form.create()(ContactForm);
