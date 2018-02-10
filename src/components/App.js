2554402c20bfb19459da0a783fe480ac04853ef7
Zachary E. Sohovich <zacharyesohovich@gmail.com>
2018-02-09 21:34:37 -0800

    eslint updates

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import moment from 'moment';
-import { Icon, Button } from 'antd';
-import me from '../assets/me.JPG';
-import './App.css';
+import React, { Component } from "react";
+import moment from "moment";
+import { Icon, Button } from "antd";
+import me from "../assets/me.JPG";
+import "./App.css";
 
 class App extends Component {
   constructor() {
@@ -10,22 +10,22 @@ class App extends Component {
     this.state = {
       socials: [
         {
-          name: 'Code',
-          link: 'https://github.com/sneakycr0w',
-          icon: 'github',
+          name: "Code",
+          link: "https://github.com/sneakycr0w",
+          icon: "github"
         },
         {
-          name: 'Contact',
-          link: 'mailto:zach@sneakycr0w.com',
-          icon: 'mail'
+          name: "Contact",
+          link: "mailto:zach@sneakycr0w.com",
+          icon: "mail"
         },
         {
-          name: 'Resume',
-          link: 'https://www.linkedin.com/in/zachary-sohovich',
-          icon: 'linkedin'
+          name: "Resume",
+          link: "https://www.linkedin.com/in/zachary-sohovich",
+          icon: "linkedin"
         }
       ]
-    }
+    };
   }
 
   render() {
@@ -37,7 +37,11 @@ class App extends Component {
           <h4>React Developer, Portland, OR</h4>
         </header>
         <section className="bio">
-          <p>I am a {moment("19930601", "YYYYDDMM").fromNow(true)} old developer from Portland, OR. I like working with current technologies such as React, Node.js, MongoDB, and more.</p>
+          <p>
+            I am a {moment("19930601", "YYYYDDMM").fromNow(true)} old developer
+            from Portland, OR. I like working with current technologies such as
+            React, Node.js, MongoDB, and more.
+          </p>
           {this.state.socials.map((social, index) => (
             <Button href={social.link} style={{ margin: "5px" }} key={index}>
               <Icon type={social.icon} key={index} /> {social.name}
@@ -45,10 +49,13 @@ class App extends Component {
           ))}
         </section>
         <footer>
-          <p className="small">Coded with <Icon type="heart" style={{ color: "red" }}/> on <a href="https://github.com/sneakycr0w/sneakycr0w.com">Github</a></p>
+          <p className="small">
+            Coded with <Icon type="heart" style={{ color: "red" }} /> on{" "}
+            <a href="https://github.com/sneakycr0w/sneakycr0w.com">Github</a>
+          </p>
         </footer>
       </div>
-    )
+    );
   }
 }
 
--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,65 +1,66 @@
 import React, { Component } from 'react';
 import { Form, Input, Icon, Button, Row, Col } from 'antd';
+import propTypes from 'prop-types';
+
 const FormItem = Form.Item;
 const { TextArea } = Input;
 
 class ContactForm extends Component {
-
   constructor() {
     super();
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit = (e) => {
+  handleSubmit = e => {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
       if (!err) {
         console.log('Received values of form: ', values);
       }
     });
-  }
+  };
 
   render() {
     const { getFieldDecorator } = this.props.form;
     return (
       <Form onSubmit={this.handleSubmit}>
-      <Row gutter={16}>
-        <Col span={6}>
-          <FormItem>
-            {getFieldDecorator('email', {
-              rules: [{ required: true, message: 'Please input your email!' }],
-            })(
-              <Input prefix={<Icon type="mail" />} placeholder="Email" />
-            )}
-          </FormItem>
-        </Col>
-        <Col span={6}>
-          <FormItem>
-            {getFieldDecorator('name', {
-              rules: [{ required: true, message: 'Please input your name!'}],
-            })(
-              <Input prefix={<Icon type="user" />} placeholder="Name" />
-            )}
-          </FormItem>
-        </Col>
-      </Row>
+        <Row gutter={16}>
+          <Col span={6}>
+            <FormItem>
+              {getFieldDecorator('name', {
+                rules: [{ required: true, message: 'Please input your name!' }],
+              })(<Input prefix={<Icon type="user" />} placeholder="Name" />)}
+            </FormItem>
+          </Col>
+          <Col span={6}>
+            <FormItem>
+              {getFieldDecorator('email', {
+                rules: [{ required: true, message: 'Please input your email!' }],
+              })(<Input prefix={<Icon type="mail" />} placeholder="Email" />)}
+            </FormItem>
+          </Col>
+        </Row>
         <Row gutter={16}>
           <Col span={12}>
             <FormItem>
               {getFieldDecorator('message', {
-                rules: [{ required: true, message: 'Please input a message!'}],
-              })(
-                <TextArea rows={4} placeholder="Message" />
-              )}
+                rules: [{ required: true, message: 'Please input your message!' }],
+              })(<TextArea rows={4} placeholder="Message" />)}
             </FormItem>
             <FormItem>
-              <Button type="primary" htmlType="submit">Submit</Button>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
             </FormItem>
           </Col>
         </Row>
       </Form>
-    )
+    );
   }
 }
+
+ContactForm.propTypes = {
+  form: propTypes.object.isRequired,
+};
 
 export default Form.create()(ContactForm);
